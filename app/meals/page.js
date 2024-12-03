1188dd6e35dbe8ef46183ab7421199354a9e5b21
Arun Prasad Anbalagan <aarunprasad92@gmail.com>
2024-12-03 23:12:26 +0530

    Add routes

---
+++ app/meals/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MealsPage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Meals Page</h1>
+      <p>This is a Meals Page</p>
+    </div>
+  );
+};
+
+export default MealsPage;
---
+++ app/meals/share/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MealsSharePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Meals Share Page</h1>
+      <p>This is a Meals Share Page</p>
+    </div>
+  );
+};
+
+export default MealsSharePage;
