4819c14e6c7ef01b3c9c792874cde6ba5e7b6aca
Arun Prasad Anbalagan <aarunprasad92@gmail.com>
2024-12-05 23:19:29 +0530

    Get data from local db

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,8 +2,10 @@ import React from 'react';
 import classes from './page.module.css';
 import Link from 'next/link';
 import MealsGrid from '../components/meals/mealsGrid';
+import { getMeals } from '../../lib/meals';
 
-const MealsPage = () => {
+const MealsPage = async () => {
+  const meals = await getMeals();
   return (
     <>
     <header className={classes.header}>
@@ -21,7 +23,7 @@ const MealsPage = () => {
       </p>
     </header>
     <main className={classes.main}></main>
-      <MealsGrid meals={[]} />
+      <MealsGrid meals={meals} />
     </>
   );
 };
