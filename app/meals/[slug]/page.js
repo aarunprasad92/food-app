f8f9b385de245657616a5e9ef45a749fc17ebe6f
Arun Prasad Anbalagan <aarunprasad92@gmail.com>
2024-12-06 15:57:33 +0530

    Usage of notFound() method to handle no data

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,9 +1,15 @@
 import Image from 'next/image';
 import classes from './page.module.css';
 import { getMealById } from '@/lib/meals';
+import { notFound } from 'next/navigation';
 
 const MealPage = ({params}) => {
   const meal = getMealById(params.slug);
+  
+  if (!meal) {
+    notFound();
+  }
+  
   meal.instructions = meal.instructions.replace(/\n/g, '<br>');
   return <>
     <header className={classes.header}>
