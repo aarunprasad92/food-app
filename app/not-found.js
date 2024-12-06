44a7ca773e1e4cace125aec384174ccdab05ae47
Arun Prasad Anbalagan <aarunprasad92@gmail.com>
2024-12-06 11:36:53 +0530

    Add a not found built in feature

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not found</h1>
+      <p>Unfortunately the requested page is not found</p>
+    </main>
+  );
+}
