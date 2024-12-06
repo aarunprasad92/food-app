78fbc7347ee415e0d1c28514c83dca3a4c945e33
Arun Prasad Anbalagan <aarunprasad92@gmail.com>
2024-12-06 16:41:12 +0530

    Custom image picker button

--- app/components/meals/imagePicker.js
+++ app/components/meals/imagePicker.js
@@ -1,10 +1,25 @@
+'use client';
+
+import { useRef } from 'react';
 import classes from './imagePicker.module.css';
 
 export default function ImagePicker({label, name}) {
+  const imageInput = useRef();
+
+  function handlePickImage() {
+    imageInput.current.click();
+  }
+
   return <div className={classes.picker}>
     <label htmlFor={name}>{label}</label>
     <div className={classes.controls}>
-      <input type="file" id={name} name={name} accept="image/png, image/jpeg" />
+      <input
+        className={classes.input} 
+        ref={imageInput}
+        type="file" id={name} name={name} accept="image/png, image/jpeg" />
+      <button className={classes.button} type="button" onClick={handlePickImage}>
+        Pick an Image
+      </button>
     </div>
   </div>
 }
