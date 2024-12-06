d17eb519554eb1b3838253f1e7ae7da2a07afcad
Arun Prasad Anbalagan <aarunprasad92@gmail.com>
2024-12-06 23:08:52 +0530

    useFormStatus to change button state while submitting

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,6 +1,7 @@
 import ImagePicker from '@/app/components/meals/imagePicker';
 import classes from './page.module.css';
 import { shareMeal } from '@/lib/actions';
+import MealsFormSubmit from '@/app/components/meals/mealsFormSubmit';
 
 export default function ShareMealPage() {
   return (
@@ -42,7 +43,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Your Image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
